dd35bdb6c38a6625bec0be7cc1898ed8196ee1ef
Gillian-LHorset <pq60soi@eduvaud.ch>
2026-06-12 10:23:00 +0200

    fix(frontend): Recréation du projet pour résoudre les problèmes
    [120][DONE]

--- Frontend/README.md
+++ Frontend/README.md
@@ -1,4 +1,4 @@
-# rustodo-front
+# Frontend
 
 This template should help get you started developing with Vue 3 in Vite.
 
---
+++ Frontend/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <h1>Hello world !</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+};
+</script>
+
+<style scoped></style>
